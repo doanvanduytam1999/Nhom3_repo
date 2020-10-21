7d09643a5afb20f4fae1405fcfa2982b27964662
julsu2905 <Administrator@P9_23>
2020-10-21 16:20:38 +0700

    add module Cong 2 so

--- BTgit/BTgit/Header.h
+++ BTgit/BTgit/Header.h
@@ -1,3 +1,10 @@
 #pragma once
 #include<iostream>
+#include<ctime>
+#include <cstdlib>
+#include<math.h>
 using namespace std;
+
+bool kiemTraCong(int soCong1, int soCong2, int ketQua);
+
+int tao2SoNgauNhien(int &so2);
---
+++ BTgit/BTgit/moduleCong2So.cpp
@@ -0,0 +1,12 @@
+#include"Header.h"
+
+bool kiemTraCong(int soCong1, int soCong2, float ketQua)
+{
+	return soCong1 + soCong2 == ketQua;
+}
+
+int tao2SoNgauNhien(int &so2) {
+	srand(time(NULL));
+	so2 = rand() % 100 + 1;
+	return rand() % 100 + 1;
+}
