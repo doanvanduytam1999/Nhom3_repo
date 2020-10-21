40d067b47ec2f63f508593219f1a98e30d7ac65e
doanvanduytam1999 <44457969+doanvanduytam1999@users.noreply.github.com>
2020-10-21 16:11:28 +0700

    first commit

---
+++ BTgit/BTgit/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+int tong(int a, int b)
+{
+	return a + b;
+}
+int hieu(int a, int b) {
+	return a - b;
+}
+int tich(int a, int b)
+{
+	return a * b;
+}
+float thuong(int a, int b)
+{
+	if (b != 0)
+		return a / b;
+	return -9999999999999;
+}
+int main()
+{
+	int chon;
+	cout << "Nhap 1 de tinh phep tinh co ban!" << endl;
+	cout << "Nhap 2 de tinh chu vi va dien tich!" << endl;
+	cout << "Nhap 3 de luyen tap phep cong!" << endl;
+	cout << "Nhap 4 de luyen tap phep tru!" << endl;
+	cout << "Moi ban chon: ";
+	cin >> chon;
+
+	switch (chon)
+	{
+	case 1:
+		int a, b;
+		char phepToan;
+
+		cout << "Nhap 2 so nguyen: ";
+		cin >> a >> b;
+		cout << "Tinh gi? (+,-,*,/) ";
+		cin >> phepToan;
+		switch (phepToan)
+		{
+		case '+':
+			cout << "Tong :" << tong(a, b);
+			break;
+		case '-':
+			cout << "Hieu :" << hieu(a, b);
+			break;
+		case '*':
+			cout << "Tich :" << tich(a, b);
+			break;
+		case '/':
+			if (-9999999999999 != thuong(a, b))
+				cout << "Thuong :" << thuong(a, b);
+			else
+				cout << "phep tinh chia het cho 0";
+			break;
+		default:
+			cout << "Chon sai phep toan";
+			break;
+		}
+		break;
+	case 2:
+		break;
+	case 3:
+		break;
+	case 4:
+		break;
+
+	default:
+		cout << "vui long chon chuc nang!" << endl;
+		break;
+	}
+
+
+	return 0;
+}
